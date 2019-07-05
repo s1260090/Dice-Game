9e74865787c6a65ac157c1b9b1ef9feaa6593080
恩田萌 <ondamegumi@wlan-napt-003.u-aizu.ac.jp>
2019-07-05 12:10:53 +0900

    data-mana-ex.4

--- DiceGame.c
+++ DiceGame.c
@@ -1,1 +1,14 @@
-//File
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int x, y;
+printf("Rilling the dice...\n");
+x = rand();
+y = rand();
+printf("Die 1: %d\n", x);
+printf("Die 2: %d\n", y);
+printf("Total value: %d\n", x+y);
+return 0;
+}
+
