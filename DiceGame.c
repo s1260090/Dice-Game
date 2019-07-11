14ca02a1877da8ebfb922eaafb1fcb4261a2c11e
恩田萌 <ondamegumi@ondamoenoMacBook-puro.local>
2019-07-11 22:52:52 +0900

    my first commit

--- DiceGame.c
+++ DiceGame.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
-int x, y;
-printf("Rilling the dice...\n");
-x = rand();
-y = rand();
-printf("Die 1: %d\n", x);
-printf("Die 2: %d\n", y);
-printf("Total value: %d\n", x+y);
-return 0;
+  int x, y;
+  printf("Rolling the dice...\n");
+  srand((unsigned) time(NULL));
+  x = rand()%6+1;
+  y = rand()%6+1;
+  printf("Die 1: %d\n", x);
+  printf("Die 2: %d\n", y);
+  printf("Total value: %d\n", x+y);
+  return 0;
 }
-
